8c0c258a8c475a1d117c599737243240045e4417
G-TUNENEXTGEAR-\takoi <tako.ishihata@gmail.com>
2021-01-30 16:04:47 +0900

    mobile phone gauge.

--- Assets/Script/GameUI/GameUI.cs
+++ Assets/Script/GameUI/GameUI.cs
@@ -1,22 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class GameUI : MonoBehaviour
 {
-    [SerializeField] GameObject objMobilePrefab;
-    GameObject objMobile;
-    TextMeshProUGUI tmproText;
+    [SerializeField] GameObject objMobile;
+    [SerializeField] TextMeshProUGUI tmproText;
+    [SerializeField] Image objImage;
+    [SerializeField] int lowPower;
+    [SerializeField] GameObject objPlayer;
 
     // Start is called before the first frame update
     void Start()
     {
-        objMobile = Instantiate(objMobilePrefab, Vector3.zero, Quaternion.identity);
-
-        //GameObject obj = GameObject.Find("UI_Battery_FILL_Function");
-        //obj.GetComponent<TextMeshPro>().text = "100%";
-        //tmproText.name = "100%";
+        SetGauge(200);
 
         objMobile.SetActive(false);
     }
@@ -24,6 +23,10 @@ public class GameUI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // バッテリー更新
+        int battery = objPlayer.GetComponent<PlayerSystem>().GetBattery();
+        SetGauge(battery);
+
         if(Input.GetKeyDown(KeyCode.Return))
         {
             objMobile.SetActive(true);
@@ -31,6 +34,25 @@ public class GameUI : MonoBehaviour
         if(Input.GetKeyUp(KeyCode.Return))
         {
             objMobile.SetActive(false);
+        }
+    }
+
+    void SetGauge(int power)
+    {
+        int dispPower = (int)(power / 10);
+        float dispBarLength = (float)power / 1000.0f;
+
+        tmproText.text = dispPower.ToString() + "%";
+
+        objImage.fillAmount = dispBarLength;
+
+        if(power <= lowPower)
+        {
+            objImage.color = new Color(1.0f, 0.0f, 0.0f, 1.0f);
+        }
+        else
+        {
+            objImage.color = new Color(0.0f, 1.0f, 0.0f, 1.0f);
         }
     }
 }
