31eb947a8795599090e547fe3def5b555d32448f
G-TUNENEXTGEAR-\takoi <tako.ishihata@gmail.com>
2021-01-30 14:30:49 +0900

    Create GameUI prefab.

---
+++ Assets/Script/GameUI/GameUI.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameUI : MonoBehaviour
+{
+    [SerializeField] GameObject objMobilePrefab;
+    GameObject objMobile;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        objMobile = Instantiate(objMobilePrefab, Vector3.zero, Quaternion.identity);
+        objMobile.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Return))
+        {
+            objMobile.SetActive(true);
+        }
+        if(Input.GetKeyUp(KeyCode.Return))
+        {
+            objMobile.SetActive(false);
+        }
+    }
+}
