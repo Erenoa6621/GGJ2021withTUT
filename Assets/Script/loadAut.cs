c1e6be196d35af3617ec39d0d969be56a924f0fc
ERENOA <m011823932@edu.teu.ac.jp>
2021-01-30 13:58:08 +0900

    PlayerFix

--- Assets/Script/loadAut.cs
+++ Assets/Script/loadAut.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class loadAut : MonoBehaviour
 {
     public bool loadLost = false;
+    public int check = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,21 +15,39 @@ public class loadAut : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-      
-    }
 
+
+    }
 
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "floor")
-        {
-            loadLost = true;
-        }
         if (collision.gameObject.tag == "Load")
         {
             loadLost = false;
+        }       
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Load")
+        {
+            Exit();
         }
-       
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Load")
+        {
+            check++;
+        }
+    }
+
+    void Exit()
+    {
+        check -= 1;
+        if (check < 1)
+        {
+            loadLost = true;
+        }
 
     }
 
